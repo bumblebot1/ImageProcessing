56c3e7b6ca41c4b231221d58ed62ce5363bad925
bumblebot1 <rm14834@bristol.ac.uk>
2016-11-18 11:55:31 +0000

    added dartboard to makefile

---
+++ CourseWork/dartboard.cpp
@@ -0,0 +1,13 @@
+/*Opencv headers*/
+#include<opencv2/objdetect/objdetect.hpp>
+#include<opencv2/opencv.hpp>
+#include<opencv2/core/core.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+
+/*Other headers*/
+#include<iostream>
+
+int main( int argc, const char** argv ){
+  return 0;
+}
